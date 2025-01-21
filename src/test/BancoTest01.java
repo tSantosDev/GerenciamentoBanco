ee63598cb879fcd2b9c0679f0f71b97cb5b457fe
thiago.ferreira <thiago.estudo1603@gmail.com>
2025-01-21 11:51:28 -0300

    Inicio dos testes

--- src/test/BancoTest01.java
+++ src/test/BancoTest01.java
@@ -9,8 +9,25 @@ public class BancoTest01 {
         Cliente cliente1 = new Cliente("Thiago", "07412153126");
         Cliente cliente2 = new Cliente("Giovanna", "42601573985");
 
-        Conta conta = new ContaCorrente("3601254", cliente1, 2500);
-        Conta conta1 = new ContaPoupanca("1054693", cliente1, 3650);
-        Conta conta2 = new ContaInvestimento("4726518", cliente1, 0);
+        Conta conta1 = new ContaCorrente("3601254", cliente1, 2500);
+        Conta conta2 = new ContaPoupanca("1054693", cliente1, 3650);
+        Conta conta3 = new ContaInvestimento("4726518", cliente1, 0);
+
+        banco.adicionarCliente(cliente1);
+        banco.adicionarCliente(cliente2);
+        banco.adicionarConta(conta1);
+        banco.adicionarConta(conta2);
+        banco.adicionarConta(conta3);
+
+        Cliente clienteBuscado = banco.bucarClientePorNome("Jorge");
+
+        if (clienteBuscado == null) {
+            System.out.println("Cliente não encontrado, tente novamente!");
+        } else {
+            System.out.println("Cliente encontrado!");
+            System.out.println(clienteBuscado.getNome());
+            System.out.println(clienteBuscado.getCpf());
+        }
+
     }
 }
